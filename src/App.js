a36247da91bca6146f99dfb80f353d5750865814
TAKAHISA HIRANO <hiratto@me.com>
2022-05-05 14:33:42 +0900

    Resultsの雛形作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {useState} from "react";
+import countriesJson from "./countries.json";
 import './App.css';
 import TopPage from './pages/TopPage';
 
 function App() {
+  const [country,setCountry] = useState("");
+
+  const getCountryData = () => {
+    fetch(`https://api.covid19api.com/country/${country}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
   return (
     <div>
-      <TopPage />
+      <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData}/>
     </div>
   );
 }
