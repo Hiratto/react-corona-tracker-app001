f2a22f804446532a61856357162e0b4c569828f8
TAKAHISA HIRANO <hiratto@me.com>
2022-05-10 16:45:34 +0900

    top/world両方のリンク作成、CSS/HTML調整

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 
 const Card = (props) => {
   return (
-    <div>
+    <div className="card-container">
       {props.allCountriesData.map((singleData, index) =>
-        <div key={index}>
+        <div key={index} className="card">
           <h2>{singleData.Country}</h2>
           <p>新規感染者： {singleData.NewConfirmed}</p>
           <p>感染者総数： {singleData.TotalConfirmed}</p>
