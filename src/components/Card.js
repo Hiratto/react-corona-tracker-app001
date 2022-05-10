6a2da150bc9a9a676f65af908491a94a0dc95695
TAKAHISA HIRANO <hiratto@me.com>
2022-05-10 15:33:01 +0900

    getAllCountriesDataをApp.jsへ移動

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,11 @@
-import {useState} from "react";
 
-const Card = () => {
-  const [allCountriesData, setAllCountriesData] = useState([]);
-  const getAllCountriesData = () => {
-    fetch("https://api.covid19api.com/summary")
-    .then(res => res.json())
-    .then(data => setAllCountriesData(data.Countries))
-  }
+const Card = (props) => {
   return (
     <div>
       <h2>Card</h2>
-      <button onClick={getAllCountriesData}>Get All Data</button>
-      {allCountriesData.map((singleData) =>
-        <div>
+      <button onClick={props.getAllCountriesData}>Get All Data</button>
+      {props.allCountriesData.map((singleData, index) =>
+        <div key={index}>
           <h2>{singleData.Country}</h2>
           <p>新規感染者： {singleData.NewConfirmed}</p>
           <p>感染者総数： {singleData.TotalConfirmed}</p>
