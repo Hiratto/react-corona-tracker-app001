b38461d061b4b0c06ce9d685d8308709eabb8a48
TAKAHISA HIRANO <hiratto@me.com>
2022-05-11 17:51:48 +0900

    Cardを分割代入

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,14 @@
 
-const Card = (props) => {
+const Card = ({allCountriesData}) => {
   return (
     <div className="card-container">
-      {props.allCountriesData.map((singleData, index) =>
+      {allCountriesData.map((singleData, index) =>
         <div key={index} className="card">
-          <h2>{singleData.Country}</h2>
-          <p>新規感染者： {singleData.NewConfirmed}</p>
-          <p>感染者総数： {singleData.TotalConfirmed}</p>
+          <div>
+            <h2>{singleData.Country}</h2>
+            <p>新規感染者： <span>{singleData.NewConfirmed.toLocaleString()}</span></p>
+            <p>感染者総数： <span>{singleData.TotalConfirmed.toLocaleString()}</span></p>
+          </div>
         </div>
       )}
     </div>
